c3e54660df4c67c63fe500b03787cec1aa052b83
EMSAREES <112564262+EMSAREES@users.noreply.github.com>
2022-08-31 16:30:47 -0500

    Add files via upload

---
+++ suma constructor/suma constructor/Sumaclass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_constructor
+{
+    //clase
+    internal class Sumaclass
+    {
+        //campo propiedades, como es
+        private int n1;
+        private int n2;
+
+        //constructor
+        public Sumaclass(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+        //metodo, accion, lo que hace
+        public int sumar()
+        {
+            return (n1 + n2);
+        }
+
+    }
+}
